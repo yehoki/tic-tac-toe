bec147528e313ddc746c5028375846c3f3779202
yehoki <patsenn2000@gmail.com>
2023-04-03 22:36:22 +0100

    Complete Tic-tac-toe DOM functionality

--- script.js
+++ script.js
@@ -11,34 +11,37 @@ Create a function inside of the gameController which creates the empty grid,
 and in turn which creates event listeners, dictates who's turn it is and so on.
 */
 
+//------------------------------------------------------------------------------------------------------
 // players factory
+//------------------------------------------------------------------------------------------------------
 
 const Person = (name) => {
-    const sayName = () => console.log(`The name: ${name}`);
-    return { sayName };
+  const sayName = () => console.log(`The name: ${name}`);
+  return { sayName };
+};
+
+//------------------------------------------------------------------------------------------------------
+// entry factory - represents each entry that we add to the grid
+//------------------------------------------------------------------------------------------------------
+
+const entry = () => {
+  let value = 0;
+
+  const addValue = (player) => {
+    value = player;
   };
-  
-  const newName = Person("newName");
-  newName.sayName();
-  
-  // entry factory - represents each entry that we add to the grid
-  
-  const entry = () => {
-    let value = 0;
-  
-    const addValue = (player) => {
-      value = player;
-    };
-  
-    const getValue = () => value;
-  
-    return {
-      addValue,
-      getValue,
-    };
+
+  const getValue = () => value;
+
+  return {
+    getValue,
+    addValue,
   };
-  
+};
+
+//------------------------------------------------------------------------------------------------------
 // gameBoard module
+//------------------------------------------------------------------------------------------------------
 
 const gameBoard = (() => {
   const rows = 3;
@@ -55,7 +58,31 @@ const gameBoard = (() => {
 
   const printBoard = () => {
     const printed = board.map((row) => row.map((cell) => cell.getValue()));
-    console.log(printed, 'printed');
+    console.log(printed, "printed");
+  };
+
+  // gets an array of all valid moves
+  const checkValidMoves = () => {
+    const validMoves = [];
+    board.forEach((row, rowIndex) =>
+      row.forEach((col, colIndex) => {
+        if (col.getValue() === 0) {
+          validMoves.push([rowIndex.toString(), colIndex.toString()]); // toString the numbers here since we are comparing strings later
+        }
+      })
+    );
+    return validMoves;
+  };
+
+  // Checks if the move is valid
+  const isValidMove = (move) => {
+    const validMoves = JSON.stringify(checkValidMoves());
+    return validMoves.indexOf(JSON.stringify(move)) !== -1 ? true : false;
+  };
+
+  // add entry value to the board
+  const addEntry = (move, entryValue) => {
+    board[move[0]][move[1]].addValue(entryValue);
   };
 
   // retrieving the board itself
@@ -63,12 +90,21 @@ const gameBoard = (() => {
   return {
     getBoard,
     printBoard,
+    checkValidMoves,
+    isValidMove,
+    checkValidMoves,
+    addEntry,
   };
 })();
 
 gameBoard.printBoard();
+console.log(gameBoard.checkValidMoves(), "check move");
 
+console.log(gameBoard.isValidMove([0, 2]), "check moveeee");
+
+//------------------------------------------------------------------------------------------------------
 // gameController module
+//------------------------------------------------------------------------------------------------------
 
 const gameController = ((
   playerOne = "Player One",
@@ -77,11 +113,11 @@ const gameController = ((
   const players = [
     {
       name: playerOne,
-      entry: "X",
+      entryValue: 1,
     },
     {
       name: playerTwo,
-      entry: "O",
+      entryValue: 2,
     },
   ];
 
@@ -89,51 +125,92 @@ const gameController = ((
   // Implementing a coin toss/randomness function to dictate who goes first here is an idea.
   let currentPlayer = players[0];
 
+  const getCurrentPlayer = () => currentPlayer;
+
   // When run, checks which player it is, and switches to the other player in the game
   const switchTurn = () => {
     currentPlayer = currentPlayer === players[0] ? players[1] : players[0];
   };
 
-  const getCurrentPlayer = () => currentPlayer;
+  //   const makeNewRound = () => {};
 
-  const makeNewRound = () => {};
+  return {
+    getCurrentPlayer,
+    switchTurn,
+  };
 })();
 
-
-/* screenController module 
-    Used to display the current information in the DOM
-*/
+//------------------------------------------------------------------------------------------------------
+// screenController module
+// Used to display the current information in the DOM
+//------------------------------------------------------------------------------------------------------
 
 const screenController = (() => {
   const board = gameBoard.getBoard();
   const boardGrid = document.querySelector(".game-board");
+  const main = document.querySelector('main');
+  const turnMessage = document.createElement('p');
+  main.appendChild(turnMessage);
+  
 
-  boardGrid.textContent = "";
+  const getEntry = (value) => {
+    return value === 0 ? "" : toEntry(value);
+  };
 
-  board.forEach((row, rowIndex) => {
-    row.forEach((col, colIndex) => {
-      const entryButton = document.createElement("button");
-      entryButton.classList.add("entry");
-      entryButton.dataset.row = rowIndex;
-      entryButton.dataset.column = colIndex;
-      entryButton.textContent = col.getValue();
-      boardGrid.appendChild(entryButton);
+  const toEntry = (value) => {
+    return value === 1 ? "X" : "O";
+  };
+
+  const makeBoard = () => {
+    boardGrid.textContent = "";
+    board.forEach((row, rowIndex) => {
+      row.forEach((col, colIndex) => {
+        const entryButton = document.createElement("button");
+        entryButton.classList.add("entry");
+        entryButton.dataset.row = rowIndex;
+        entryButton.dataset.column = colIndex;
+        entryButton.textContent = getEntry(col.getValue());
+        boardGrid.appendChild(entryButton);
+        console.log(col.getValue(), col.getValue() === 0);
+      });
     });
-  });
+    turnMessage.textContent = `It is ${gameController.getCurrentPlayer().name}'s turn.`;
+  };
 
   const handleGridClick = (e) => {
     const rowIndex = e.target.dataset.row;
     const colIndex = e.target.dataset.column;
 
     if (!rowIndex || !colIndex) return;
-    console.log('Button PRESS!');
-
+    console.log(
+      gameBoard.isValidMove([rowIndex, colIndex]),
+      "check",
+      rowIndex,
+      colIndex,
+      gameBoard.checkValidMoves()
+    );
+    if (gameBoard.isValidMove([rowIndex, colIndex])) {
+      console.log(gameController.getCurrentPlayer());
+      gameBoard.addEntry(
+        [rowIndex, colIndex],
+        gameController.getCurrentPlayer().entryValue
+      );
+      gameController.switchTurn();
+      makeBoard();
+      // change the value on the button;
+    } else {
+        alert('This is not a valid input!');
+    }
     //Functionality here what happens after button press
-  }
+  };
 
-
-  boardGrid.addEventListener('click', handleGridClick);
+  boardGrid.addEventListener("click", handleGridClick);
 
   console.log(board, "screenControl");
+
+  return {
+    makeBoard
+  }
 })();
 
+screenController.makeBoard();
